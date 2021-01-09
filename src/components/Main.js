471ab9065f674a887b81033128faa2127aaba1ca
ParkerCatz <tksm.webdesign09@gmail.com>
2021-01-09 20:44:56 +0900

    feat: サーバーから画像を取得して状態変更

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { fetchImages } from '../api/DogAPI'
 import Gallery from './Gallery'
 
 const Main = () => {
-  const urls = [
-    'https://images.dog.ceo/breeds/shiba/shiba-11.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-12.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-14.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-17.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-2.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-3i.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-4.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-5.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-6.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-7.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-8.jpg',
-    'https://images.dog.ceo/breeds/shiba/shiba-9.jpg',
-  ]
-  // const urls = null
+  const [urls, setUrls] = useState(null)
+  useEffect(() => {
+    fetchImages('shiba').then((urls) => {
+      setUrls(urls)
+    })
+  }, [])
   return (
     <main>
       <section className="section">
